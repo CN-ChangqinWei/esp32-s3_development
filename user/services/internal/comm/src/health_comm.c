908ef0a81c60b3cfd108e2bef81e6607076d18e4
CN-ChangqinWei <2296195685@qq.com>
2026-04-21 22:45:54 +0800

    fix:修复comm使用的不是抽象层包装问题

--- user/services/internal/comm/src/health_comm.c
+++ user/services/internal/comm/src/health_comm.c
@@ -1,14 +1,15 @@
 #include "health_comm.h"
 #include "health_service.h"
 #include "service.h"
-#include "serial_proto.h"
+#include "proto.h"
 
 uint16_t HealthCommHandler(void* instance, void* arg) {
+    (void)arg;  // 未使用参数
     Service* service = (Service*)instance;
     int len;
     char* res = HealthExec(&len);
-    if (service != NULL && service->proto != NULL) {
-        SerialProtoSendPackage(service->proto, (char*)res, len);
+    if (service != NULL && service->proto != NULL && res != NULL && len > 0) {
+        ProtoSendPackage(service->proto, res, len);
     }
     return 0;
 }
--- user/services/internal/comm/src/motor_comm.c
+++ user/services/internal/comm/src/motor_comm.c
@@ -2,14 +2,14 @@
 #include "service.h"
 #include "motor_service.h"
 #include "motor_domain.h"
-#include "serial_proto.h"
+#include "proto.h"
 
 uint16_t MotorHandler(void* instance, void* arg) {
     Service* service = (Service*)instance;
     int res = MotorExec(service, arg);
     MotorDomainReply re = {PROTO_MOTOR, res};
     if (service != NULL && service->proto != NULL) {
-        SerialProtoSendPackage(service->proto, (char*)&re, sizeof(MotorDomainReply));
+        ProtoSendPackage(service->proto, (char*)&re, sizeof(MotorDomainReply));
     }
     return 0;
 }
